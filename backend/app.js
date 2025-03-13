478cf722cf8ffdf436e8301c90ae0d28fb6875bc
jeelpatel-git <jeelpatel6@gmail.com>
2025-03-13 16:08:35 +0530

    intial commit

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path")
+const postsRoutes = require("./routes/posts");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://Jeel:JeelMongoDB703@cluster0.aqoiy.mongodb.net/node-angular?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+  }
+)
+.then(() => console.log("Connected to database!"))
+.catch(err => console.error("Connection failed!", err));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts", postsRoutes);
+
+module.exports = app;
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, NgForm, Validators } from "@angular/forms";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+
+import { PostsService } from "../posts.service";
+import { Post } from "../post.model";
+
+import { mimeType } from "./mime-type.validators"
+
+@Component({
+  selector: "app-post-create",
+  templateUrl: "./post-create.component.html",
+  styleUrls: ["./post-create.component.css"]
+})
+export class PostCreateComponent implements OnInit {
+  enteredTitle = "";
+  enteredContent = "";
+  post: Post;
+  isLoading = false;
+  private mode = "create";
+  private postId: string;
+  form:FormGroup;
+  imagePreview:string;
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute,
+    public fb: FormBuilder
+  ) {}
+
+  ngOnInit() {
+
+    this.form = this.fb.group({
+      title:["", [Validators.required, Validators.minLength(3)]],
+      content:["", Validators.required],
+      image:[null, [Validators.required], [mimeType]]
+    })
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has("postId")) {
+        this.mode = "edit";
+        this.postId = paramMap.get("postId");
+        this.isLoading = true;
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.isLoading = false;
+          this.post = {id: postData._id, title: postData.title, content: postData.content, imagePath:null};
+          this.form.setValue({
+            title: postData.title,
+            content: postData.content,
+            image: postData.imagePath
+          })
+        });
+      } else {
+        this.mode = "create";
+        this.postId = null;
+      }
+    });
+  }
+
+  onImagePicked(event:Event){
+    debugger
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image:file});
+    this.form.get('image').updateValueAndValidity();
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string
+    }
+    reader.readAsDataURL(file);
+  }
+  onSavePost() {
+    if (this.form.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    if (this.mode === "create") {
+      this.postsService.addPost(this.form.value.title, this.form.value.content, this.form.value.image);
+    } else {
+      this.postsService.updatePost(
+        this.postId,
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.image
+      );
+    }
+    this.form.reset();
+  }
+}
